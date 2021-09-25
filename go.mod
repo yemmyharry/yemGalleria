d83f6fbb61591d908d573ba50d5b19bd77b875d6
yemmyharry <yemmyharry@gmail.com>
2021-09-25 19:13:07 +0100

    added 404 page

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module calhounProject
 go 1.17
 
 require (
+	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/howeyc/fsnotify v0.9.0 // indirect
 	github.com/mattn/go-colorable v0.1.10 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
--- main.go
+++ main.go
@@ -2,15 +2,29 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("page visited")
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, `<h2> testing </h2>`)
+}
+
+func notFound(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h3>sorry but an error occured.</h3>")
+}
+
 func main()  {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("page visited")
-		w.Header().Set("Content-Type", "text/html")
-		fmt.Fprint(w, `<h2> testing </h2>`)
-	})
-	http.ListenAndServe(":8080", nil)
+	r := mux.NewRouter()
+
+	r.NotFoundHandler = http.HandlerFunc(notFound)
+	r.HandleFunc("/", Home)
+
+
+	http.ListenAndServe(":8080", r)
 }
