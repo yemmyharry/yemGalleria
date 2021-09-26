a0e06544573b47fbbf574118f536632cde05859b
yemmyharry <yemmyharry@gmail.com>
2021-09-26 16:46:34 +0100

    implemented view type in main.go

--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"calhounProject/views"
 	"fmt"
 	"github.com/gorilla/mux"
+	"log"
 	"net/http"
+)
+
+var (
+	contactView *views.View
+	homeView *views.View
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("page visited")
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, `<h2> testing </h2>`)
+	err := homeView.Template.Execute(w, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Contact(w	http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-Type", "text/html")
+	if err := contactView.Template.Execute(w,nil); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func notFound(w http.ResponseWriter, r *http.Request)  {
@@ -19,12 +36,12 @@ func notFound(w http.ResponseWriter, r *http.Request)  {
 }
 
 func main()  {
+	homeView = views.NewView("views/home.gohtml")
+	contactView = views.NewView("views/contact.gohtml")
 
 	r := mux.NewRouter()
-
 	r.NotFoundHandler = http.HandlerFunc(notFound)
 	r.HandleFunc("/", Home)
-
-
+	r.HandleFunc("/contact", Contact)
 	http.ListenAndServe(":8080", r)
 }
