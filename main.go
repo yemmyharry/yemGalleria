c88349643d13e4515484bb9ede4b93b599e64a3e
yemmyharry <yemmyharry@gmail.com>
2021-10-12 22:41:07 +0100

    made view changes

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ var (
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("page visited")
 	w.Header().Set("Content-Type", "text/html")
-	err := homeView.Template.Execute(w, nil)
+	err := homeView.Template.ExecuteTemplate(w, homeView.Layout,nil)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,7 +24,7 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 func Contact(w	http.ResponseWriter, r *http.Request)  {
 	w.Header().Set("Content-Type", "text/html")
-	if err := contactView.Template.Execute(w,nil); err != nil {
+	if err := contactView.Template.ExecuteTemplate(w, contactView.Layout,nil); err != nil {
 		log.Fatal(err)
 	}
 }
@@ -32,12 +32,12 @@ func Contact(w	http.ResponseWriter, r *http.Request)  {
 func notFound(w http.ResponseWriter, r *http.Request)  {
 	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprint(w, "<h3>sorry but an error occured.</h3>")
+	fmt.Fprint(w, "<h3>sorry but an error occurred.</h3>")
 }
 
 func main()  {
-	homeView = views.NewView("views/home.gohtml")
-	contactView = views.NewView("views/contact.gohtml")
+	homeView = views.NewView("bootstrap", "views/home.gohtml")
+	contactView = views.NewView("bootstrap","views/contact.gohtml")
 
 	r := mux.NewRouter()
 	r.NotFoundHandler = http.HandlerFunc(notFound)
