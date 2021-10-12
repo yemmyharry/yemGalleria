aa1edbaac768a192810ee52f1eaf0a18aa2ce8b4
yemmyharry <yemmyharry@gmail.com>
2021-10-12 22:40:21 +0100

    added layout field to view struct

--- views/view.go
+++ views/view.go
@@ -7,10 +7,11 @@ import (
 
 type View struct {
 	Template *template.Template
+	Layout string
 }
 
-func NewView(files ...string) *View {
-	files = append(files, "views/layouts/footer.gohtml")
+func NewView(layout string, files ...string) *View {
+	files = append(files, "views/layouts/footer.gohtml", "views/layouts/bootstrap.gohtml")
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Fatal(err)
@@ -18,5 +19,6 @@ func NewView(files ...string) *View {
 
 	return &View{
 		Template: t,
+		Layout: layout,
 	}
 }
